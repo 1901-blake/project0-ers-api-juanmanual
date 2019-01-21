ba4c031f76e29be8a67a187b0aeb81180d0bcaf8
juan0melendez <jmelendez2+code@cub.uca.edu>
2019-01-20 19:56:49 -0500

    added login functionality

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 import express from 'express'
 import { userRouter } from './routers/users.router'
 import { reimbursementsRouter } from './routers/reimbursements.router';
+import bodyParser from 'body-parser'
 
 const app = express()
 
+// log incoming requests
+app.use((req, res, next) => {
+  console.log(`request was made with url: ${req.path}
+  and method: ${req.method}`)
+  next()
+})
+
+// parse the body
+app.use(bodyParser.json())
+
+// handle login requests
+app.post('/login', (req, res, next) => {
+  let username = req.body.username
+  let password = req.body.password
+  
+
+})
 
 // register routers
 app.use('/users', userRouter)
