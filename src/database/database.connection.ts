c0f1c2bcda3060574921353d21022d621cc82a96
juan0melendez <jmelendez2+code@cub.uca.edu>
2019-01-27 12:27:20 -0500

    updated architecture to use connection factory

--- src/database/database.connection.ts
+++
@@ -1,8 +0,0 @@
-import {Client} from 'pg'
-import { connectionString } from './.database.config';
-/**
- * allows accessing the database
- * where the models are stored
- */
-export let client = new Client(connectionString)
-
