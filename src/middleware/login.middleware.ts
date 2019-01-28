1bd55fcf28fc2a6c5ddb35bac88dc1f0300b889e
juan0melendez <jmelendez2+code@cub.uca.edu>
2019-01-27 19:50:47 -0500

    login now attaches user info to session

--- src/middleware/login.middleware.ts
+++ src/middleware/login.middleware.ts
@@ -1,16 +1,18 @@
 import {UserDao} from '../dao/user-dao'
-export const login = (req, res, next) => {
+export const login = (req, res ) => {
   let username = req.body.username;
   let password = req.body.password;
 
   let userDao = new UserDao();
 
   try {
-    req.user = userDao.getUserByLoginInfo(username, password);
-    next()
+    const user = userDao.getUserByLoginInfo(username, password);
+    req.session.user = user;
+    res.json(user)
   } catch(e ){
     console.log('bad login attempt')
-    res.sendStatus(400)
+    
+    res.status(400).send('Bad login attempt')
   }
   
 }
